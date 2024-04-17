2b26693862c78b2fb5fe64b1f623f807d15a6131
Daniel Marschall <info@daniel-marschall.de>
2024-04-17 23:34:29 +0000

    Bugfix: Kommentare wurden 1 Sekunde anstelle 5 Sekunden gezeigt.
    git-svn-id: https://svn.viathinksoft.com/svn/dpg2/trunk@20 d8f833e3-d2fc-4023-84b4-3271e27272c9

--- DpgGame.pas
+++ DpgGame.pas
@@ -707,30 +707,25 @@ begin
 
   if not freegamer then
   begin
-    if Spielzeit=60 then
+    // TODO: Diese Kommentare sind doch gar nicht sichtbar, es sei denn man stoppt den Motor
+    if (Spielzeit>=60) and (Spielzeit<=65) then
       Kommentar:='Sie sollten sich etwas beeilen!';
-    if Spielzeit=65 then Kommentar:='';
-    if Spielzeit=120 then
+    if (Spielzeit>=120) and (Spielzeit<=125) then
       Kommentar:='Ihr Gegner schläft gleich ein!';
-    if Spielzeit=125 then Kommentar:='';
-    if Spielzeit=180 then
+    if (Spielzeit>=180) and (Spielzeit<=185) then
       Kommentar:='Wird das heute noch was?!';
-    if Spielzeit=185 then Kommentar:='';
-    if Spielzeit=240 then
+    if (Spielzeit>=240) and (Spielzeit<=245) then
       Kommentar:='Jetzt beeilen Sie sich endlich!';
-    if Spielzeit=245 then Kommentar:='';
-    if Spielzeit=300 then
+    if (Spielzeit>=300) and (Spielzeit<=300) then
       Kommentar:='Wir haben nicht unendlich Zeit!';
-    if Spielzeit=305 then Kommentar:='';
-    if Spielzeit=365 then
+    if (Spielzeit>=365) and (Spielzeit<=370) then
       Kommentar:='Dino: Du kriegst mich nicht, ätsch!';
-    if Spielzeit=370 then Kommentar:='';
-    if Spielzeit=430
-      then Kommentar:='Dino: schnarch... schnarch...';
-    if Spielzeit=435 then Kommentar:='';
-    if Spielzeit=495
-      then Kommentar:='LANGWEILIG!!!';
-    if Spielzeit=500 then Kommentar:='';
+    if (Spielzeit>=430) and (Spielzeit<=435) then
+      Kommentar:='Dino: schnarch... schnarch...';
+    if (Spielzeit>=495) and (Spielzeit<=500) then
+      Kommentar:='LANGWEILIG!!!';
+    else
+       Kommentar:='';
   end
   else Kommentar:='Spielen ohne Begrenzung!';
 end;
